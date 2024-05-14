67523e6903eedee47188d0b39e7e80e522641d24
Charlee2543 <Charlee2543@hotmail.com>
2024-05-14 14:17:24 +0700

    Ex1_Funtion

---
+++ Ex1_Funtion.js
@@ -0,0 +1,30 @@
+const a=8;
+const b=4;
+//...................
+
+function add(a,b){
+    return(a+b);
+}
+const answerAdd=add(a,b);
+console.log(answerAdd);
+//...................
+
+function subtract(a,b){
+    return(a-b);
+}
+const answerSubtract=subtract(a,b);
+console.log(answerSubtract);
+//...................
+
+function multiply(a,b){
+    return(a*b);
+}
+const answerMultiply=multiply(a,b);
+console.log(answerMultiply);
+//...................
+
+function divide(a,b){
+    return(a/b);
+}
+const answerDivide=divide(a,b);
+console.log(answerDivide);
